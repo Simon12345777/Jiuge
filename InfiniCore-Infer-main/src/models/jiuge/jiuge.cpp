d3db7fab71398fc7c5bddbb98dbff2f23d17529b
18321 <1832140001@qq.com>
2025-08-20 19:15:46 +0800

    fix5

--- InfiniCore-Infer-main/src/models/jiuge/jiuge.cpp
+++ InfiniCore-Infer-main/src/models/jiuge/jiuge.cpp
@@ -264,7 +264,7 @@ void inferDeviceBatch(const JiugeMeta &meta, DeviceResource &rsrc,
 
         if (past_len == 0) {
             RUN_INFINI(infiniopCreateGemmDescriptor(
-                    rsrc.handle, &desc_qk_gemms[req], qk->desc(), q_t->desc(), k->permute({1, 2, 0})->desc()));
+                    rsrc.handle, &desc_qk_gemms[req], qk->desc(), q_t->desc(), (k->permute({1, 2, 0}))->desc()));
         } else {
             RUN_INFINI(infiniopCreateGemmDescriptor(
                     rsrc.handle, &desc_qk_gemms[req], qk->desc(), q_t->desc(), full_kv->desc()));
@@ -279,7 +279,7 @@ void inferDeviceBatch(const JiugeMeta &meta, DeviceResource &rsrc,
 
         if (past_len == 0) {
             RUN_INFINI(infiniopCreateGemmDescriptor(
-                    rsrc.handle, &desc_attn_v_gemms[req], q_t->desc(), qk->desc(), v->permute({1, 0, 2})->desc()));
+                    rsrc.handle, &desc_attn_v_gemms[req], q_t->desc(), qk->desc(), (v->permute({1, 0, 2}))->desc()));
         } else {
             RUN_INFINI(infiniopCreateGemmDescriptor(
                     rsrc.handle, &desc_attn_v_gemms[req], q_t->desc(), qk->desc(), full_v->desc()));
